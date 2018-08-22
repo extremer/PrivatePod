1b2caec052bbaa9b7494cc771638a5f7d9a1330b
jihoon.jeon <jihoon.jeon@wisefour.com>
2018-08-22 17:35:09 +0900

    Initial commit.

---
+++ JJPrivatePod/Classes/TestClass.swift
@@ -0,0 +1,14 @@
+//
+//  TestClass.swift
+//  FBSnapshotTestCase
+//
+//  Created by Jihoon Jeon on 2018. 8. 22..
+//
+
+import Foundation
+
+public class TestClass {
+    static public func doTest() -> String {
+        return "Test!"
+    }
+}
